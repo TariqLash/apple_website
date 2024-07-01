90eea1b02c8e497f4714281c674e7707766eb0ae
Tariq Lashley <tariqlash@gmail.com>
2024-07-01 16:53:52 -0600

    finished quick navbar and routing for highlights and hero

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='bg-black'>
+      <Navbar/>
+      <Hero/>
+      <Highlights/>
+        
+    </main>
+  )
+}
+
+export default App
