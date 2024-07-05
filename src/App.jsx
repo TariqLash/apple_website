c759b48f0ed2d5cd70c2d17b91404f933616db15
Tariq Lashley <tariqlash@gmail.com>
2024-07-04 21:09:30 -0600

    working on 3D Model

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Highlights/>
+      <Model/>
         
     </main>
   )
